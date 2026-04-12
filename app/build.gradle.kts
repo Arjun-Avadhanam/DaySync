0b923c3a7939c6cc01ad6df3374cccbdc43f1640
Arjun-Avadhanam <arjunvijayavadhanam@gmail.com>
2026-04-12 14:46:47 +0530

    Bump version to 1.8.1 (10801)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.daysync.app"
         minSdk = 28
         targetSdk = 36
-        versionCode = 10800
-        versionName = "1.8.0"
+        versionCode = 10801
+        versionName = "1.8.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
